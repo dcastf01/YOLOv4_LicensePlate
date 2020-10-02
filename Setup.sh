bbfead38ea99f6df3a8cce251c5f966fcfb3f9e8
David Castellano Falcón <dcastf01@gmail.com>
2020-10-02 15:00:43 +0200

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,3 +1,18 @@
+# clone darknet repo
+git clone https://github.com/AlexeyAB/darknet
+# change makefile to have GPU and OPENCV enabled
+cd darknet
+sed -i 's/OPENCV=0/OPENCV=1/' Makefile
+sed -i 's/GPU=0/GPU=1/' Makefile
+sed -i 's/CUDNN=0/CUDNN=1/' Makefile
+sed -i 's/CUDNN_HALF=0/CUDNN_HALF=1/' Makefile
+# verify CUDA
+/usr/local/cuda/bin/nvcc --version
+
+make
+cd ..
+mkdir backup
+
 git clone https://github.com/dcastf01/OIDv4_ToolKit.git
 
 cd OIDv4_ToolKit/
@@ -13,34 +28,24 @@ rm -r OID/Dataset/train/Vehicle registration plate/Label/
 rm -r OID/Dataset/validation/Vehicle registration plate/Label/
 
 cd ..
+cd darknet
 #move dataset out
 mkdir data
-cp /content/OIDv4_ToolKit/OID/Dataset/train -r /content/data
-cp /content/OIDv4_ToolKit/OID/Dataset/validation -r /content/data
+cp /content/OIDv4_ToolKit/OID/Dataset/train -r /content/darknet/data
+cp /content/OIDv4_ToolKit/OID/Dataset/validation -r /content/darknet/data
 rm -rf OIDv4_ToolKit
 
 
-# clone darknet repo
-git clone https://github.com/AlexeyAB/darknet
-# change makefile to have GPU and OPENCV enabled
-cd darknet
-sed -i 's/OPENCV=0/OPENCV=1/' Makefile
-sed -i 's/GPU=0/GPU=1/' Makefile
-sed -i 's/CUDNN=0/CUDNN=1/' Makefile
-sed -i 's/CUDNN_HALF=0/CUDNN_HALF=1/' Makefile
-# verify CUDA
-/usr/local/cuda/bin/nvcc --version
-make
-
-cd ..
-mkdir backup
+mv /content/darknet/data/train /content/darknet/data/obj
+mv /content/darknet/data/validation /content/darknet/data/test
 
 
-mv /content/data/train /content/data/obj
-mv /content/data/validation /content/data/test
+
+
+
 
 #Set files of configurations
-cp /content/YOLOv4_LicensePlate/yolov4/yolov4-obj.cfg darknet/cfg
+cp /content/YOLOv4_LicensePlate/yolov4/yolov4-obj.cfg ./cfg
 cp /content/YOLOv4_LicensePlate/yolov4/obj.names ./data
 cp /content/YOLOv4_LicensePlate/yolov4/obj.data  ./data
 
