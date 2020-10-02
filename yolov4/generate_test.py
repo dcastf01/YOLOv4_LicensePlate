a7d38c8c34e90afb2a83a2cef96e85b92a68acb9
David Castellano Falcón <dcastf01@gmail.com>
2020-10-02 14:31:17 +0200

    Update generate_test.py

--- yolov4/generate_test.py
+++ yolov4/generate_test.py
@@ -2,11 +2,14 @@ import os
 
 image_files = []
 os.chdir(os.path.join("data", "test"))
-for filename in os.listdir(os.getcwd()):
-    if filename.endswith(".jpg"):
-        image_files.append("data/test/" + filename)
+for folder in os.listdir(os.getcwd()):
+  os.chdir(os.path.join(str(folder)))
+  for filename in os.listdir(os.getcwd()):
+      if filename.endswith(".jpg"):
+          image_files.append("data/test/" + filename)
+  os.chdir("..")
 os.chdir("..")
-with open("test.txt", "w") as outfile:
+with open("train.txt", "w") as outfile:
     for image in image_files:
         outfile.write(image)
         outfile.write("\n")
