244f55e94fa7f59b875fea2693560ed0eea4dbd8
David Castellano Falcón <dcastf01@gmail.com>
2020-10-02 14:31:01 +0200

    Update generate_train.py

--- yolov4/generate_train.py
+++ yolov4/generate_train.py
@@ -2,9 +2,12 @@ import os
 
 image_files = []
 os.chdir(os.path.join("data", "obj"))
-for filename in os.listdir(os.getcwd()):
-    if filename.endswith(".jpg"):
-        image_files.append("data/obj/" + filename)
+for folder in os.listdir(os.getcwd()):
+  os.chdir(os.path.join(str(folder)))
+  for filename in os.listdir(os.getcwd()):
+      if filename.endswith(".jpg"):
+          image_files.append("data/obj/" + filename)
+  os.chdir("..")
 os.chdir("..")
 with open("train.txt", "w") as outfile:
     for image in image_files:
